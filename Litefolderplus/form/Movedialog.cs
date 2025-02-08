7d79654cdd1b2c4ad926bcc16382a2e484e6cc4b
Acid <masatozhongcun808@gmail.com>
2025-02-08 18:25:44 +0900

    File copy done

---
+++ Litefolderplus/form/Movedialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Litefolderplus
+{
+    public partial class Movedialog : Form
+    {
+        public Movedialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
